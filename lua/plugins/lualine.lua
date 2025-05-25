37b3d8c159de6f10241ad2c8bba7ec4b487a7c83
Stephen Gallagher <stephen.gallagher@gmail.com>
2025-05-25 21:11:54 +0100

    Add conform

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {options = {theme = 'dracula'}},
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = { options = { theme = "dracula" } },
 }
--- lua/stephen/remap.lua
+++ lua/stephen/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {desc="LSP Format"})
+-- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {desc="LSP Format"})
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc="[P]roject [V]iew (Explorer)"})
 
