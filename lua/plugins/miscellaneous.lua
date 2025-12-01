643d85934a54a3a50f921aaec7d90040931c7e6b
Stephen Gallagher <stephen.gallagher@gmail.com>
2025-12-01 13:53:14 +0000

    disable mini surround

--- lua/plugins/miscellaneous.lua
+++ lua/plugins/miscellaneous.lua
@@ -12,29 +12,29 @@ return { -- Collection of various small independent plugins/modules
 	--
 	-- 	end,
 	-- },
-	{
-		"https://github.com/nvim-mini/mini.surround",
-		-- Add custom surroundings to be used on top of builtin ones. For more
-		-- information with examples, see `:h MiniSurround.config`.
-		custom_surroundings = nil,
-
-		-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
-		highlight_duration = 500,
-		opts = {
-			mappings = {
-				add = "sa", -- Add surrounding in Normal and Visual modes
-				delete = "sd", -- Delete surrounding
-				find = "sf", -- Find surrounding (to the right)
-				find_left = "sF", -- Find surrounding (to the left)
-				highlight = "sh", -- Highlight surrounding
-				replace = "sr", -- Replace surrounding
-
-				suffix_last = "l", -- Suffix to search with "prev" method
-				suffix_next = "n", -- Suffix to search with "next" method
-			},
-
-			-- Number of lines within which surrounding is searched
-			n_lines = 20,
-		},
-	},
+	--	{
+	--		"https://github.com/nvim-mini/mini.surround",
+	--		-- Add custom surroundings to be used on top of builtin ones. For more
+	--		-- information with examples, see `:h MiniSurround.config`.
+	--		custom_surroundings = nil,
+	--
+	--		-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+	--		highlight_duration = 500,
+	--		opts = {
+	--			mappings = {
+	--				add = "sa", -- Add surrounding in Normal and Visual modes
+	--				delete = "sd", -- Delete surrounding
+	--				find = "sf", -- Find surrounding (to the right)
+	--				find_left = "sF", -- Find surrounding (to the left)
+	--				highlight = "sh", -- Highlight surrounding
+	--				replace = "sr", -- Replace surrounding
+	--
+	--				suffix_last = "l", -- Suffix to search with "prev" method
+	--				suffix_next = "n", -- Suffix to search with "next" method
+	--			},
+	--
+	--			-- Number of lines within which surrounding is searched
+	--			n_lines = 20,
+	--		},
+	--	},
 }
