907cf99304a541f4d23514eab0f27a777608b68a
Stephen Gallagher <stephen.gallagher@gmail.com>
2026-01-31 00:37:55 +0000

    upgrade part 2

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,10 @@
+return {
+  "norcalli/nvim-colorizer.lua",
+  ft = { "css", "javascript", "html" },
+  opts = {
+    "css",
+    "javascript",
+    css = { rgb_fn = true },
+    html = { names = false },
+  },
+}
